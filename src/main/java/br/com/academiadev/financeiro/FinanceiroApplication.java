67dd68c82edb4afab46782c4ccc2e6e946c306f9
Bruno Muehlbauer de Souza <docsbruno@gmail.com>
2018-10-27 14:30:07 -0300

    Adicionado Swagger

--- src/main/java/br/com/academiadev/financeiro/FinanceiroApplication.java
+++ src/main/java/br/com/academiadev/financeiro/FinanceiroApplication.java
@@ -2,11 +2,12 @@ package br.com.academiadev.financeiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class FinanceiroApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinanceiroApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceiroApplication.class, args);
+    }
 }
